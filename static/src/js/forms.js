aa4084c29b5638dca6f448d4f88dfa7308fd868a
Russell Briggs <russell.briggs@junari.com>
2020-04-05 01:07:17 +0000

    Add initial field rendering support and CSS

--- static/src/js/forms.js
+++ static/src/js/forms.js
@@ -8,12 +8,37 @@ odoo.define('jowebutils.forms', function (require) {
 
     // var _t = core._t;
 
+    var CharField = Widget.extend({
+        template: 'jowebutils.field_char',
+        init: function (parent, field, default_value) {
+            this.state = {
+                field,
+                value: default_value
+            }
+            return this._super(parent);
+        },
+    });
+
     var WebForm = Widget.extend({
+        xmlDependencies: ['/jowebutils/static/src/xml/forms.xml'],
+
+        init: function (parent) {
+            this.state = {
+                fields: [
+                    { name: 'name', string: 'Name', type: 'char', required: true },
+                    { name: 'date', string: 'Date', type: 'date', required: false }
+                ]
+            }
+            return this._super(parent)
+        },
         start: function () {
-            console.log('web form widget started 2');
-            this.$el.html('<div>form goes here</div>');
+            // Render fields
+            this.state.fields.forEach(field => {
+                var field = new CharField(this, field);
+                field.appendTo(this.$el);
+            })
             return this._super();
-        }
+        },
     });
 
     return {
