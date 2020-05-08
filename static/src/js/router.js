07dbe64fc55f6e967ada6bd87dd3ace7ed457870
Russell Briggs <russell.briggs@junari.com>
2020-05-08 17:54:24 +0000

    Implement RouterWidget (based on jQuery router)

---
+++ static/src/js/router.js
@@ -0,0 +1,73 @@
+
+odoo.define('jowebutils.router', function (require) {
+    'use strict';
+
+    const Widget = require('web.Widget');
+
+    const RouterWidget = Widget.extend({
+        jsLibs: [
+            '/jowebutils/static/lib/deparam.js',
+            '/jowebutils/static/lib/jquery.router.js'
+        ],
+
+        init: function (parent) {
+            this.state = {
+                routes: [],
+                routeWidgets: [],
+            }
+            return this._super(parent)
+        },
+
+        addRoute: function (routeDef) {
+            this.state.routes.push(routeDef);
+        },
+
+        start: function () {
+            // Register router
+            $.route('*', (e) => {
+                console.log('routed to', e.route);
+                this._routeChange(e.route);
+            });
+            // Route to current URL
+            const l = window.location;
+            const currentUrl = l.pathname + l.search;
+            $.router.set(currentUrl);
+            return this._super();
+        },
+
+        _routeChange: function (url) {
+            let routeWidget = this.state.routeWidgets[url]
+            let routePromise = Promise.resolve();
+            if (!routeWidget) {
+                const route = this.state.routes.find(r => r.url == url)
+                if (!route) {
+                    alert('url ' + url + ' not found.'); // FIXME
+                    return;
+                }
+                routeWidget = new route.widget(this);
+                routePromise = routeWidget.appendTo(this.$el);
+            }
+            return routePromise
+                .then(() => {
+                    if (routeWidget.onRouteEnter) {
+                        return routeWidget.onRouteEnter();
+                    }
+                })
+                .then(() => {
+                    this.state.routeWidgets.forEach(widget => {
+                        if (widget != routeWidget) {
+                            widget.do_hide();
+                        }
+                        else {
+                            widget.do_show();
+                        }
+                    });
+                });
+        }
+    });
+
+    return {
+        RouterWidget
+    };
+
+});
