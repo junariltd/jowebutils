3d69384bfe00cc5ce2f70836bbcc632b9740e0af
Russell Briggs <russell.briggs@junari.com>
2021-01-29 22:01:54 +0000

    [T11114] Wire-up RPC calls. OWL apps now ready for prime time :)

--- static/src/js/owl_app.js
+++ static/src/js/owl_app.js
@@ -4,6 +4,8 @@ odoo.define('jowebutils.owl_app', function (require) {
     // Creates a OWL app entry point with the specified routes
 
     const publicWidget = require('web.public.widget');
+    const rpc = require('web.rpc');
+    const session = require('web.session');
 
     const { ComponentWrapper, WidgetAdapterMixin } = require('web.OwlCompatibility');
     const { Component, tags } = owl;
@@ -19,7 +21,23 @@ odoo.define('jowebutils.owl_app', function (require) {
 
             init: function () {
                 this.owl_component = new ComponentWrapper(this, App);
-                this.owl_component.env.router = new Router(this.owl_component.env, routes);
+                const env = this.owl_component.env;
+                env.router = new Router(this.owl_component.env, routes);
+                this.populateOWLEnv(env);
+            },
+
+            populateOWLEnv: function () {
+                // Populate OWL env from current odoo environment
+                // Try to mimic odoo 14+ where possible, to make porting easier
+                // https://github.com/odoo/odoo/blob/14.0/addons/web/static/src/js/common_env.js#L46
+
+                const env = this.owl_component.env;
+                env.services = {
+                    rpc: function(params, options) {
+                        const query = rpc.buildQuery(params);
+                        return session.rpc(query.route, query.params, options);
+                    }
+                }
             },
 
             start: function () {
