bab7ddced66ac626106910d06e632d99f86a1a83
Russell Briggs <russell.briggs@junari.com>
2021-03-13 15:04:35 +0000

    [T11706] define() Add support for "external" dependencies (e.g. OWL)

--- static/src/js/define.js
+++ static/src/js/define.js
@@ -4,19 +4,33 @@
 */
 'use strict';
 
+window.defineLibs = {};
+window.defineLibs['@odoo/owl'] = () => owl;
+
 window.define = function(moduleName, moduleReqs, impl) {
     console.debug('Registering AMD Module: ', moduleName, moduleReqs);
 
-    const odooReqs = moduleReqs.filter(r => r != 'require' && r != 'exports');
+    const odooReqs = moduleReqs.filter(r => {
+        return r != 'require' && r != 'exports' && !(r in window.defineLibs)
+    });
 
     odoo.define(moduleName, odooReqs, function(require) {
         const exports = {};
         const implArgs = [];
         moduleReqs.forEach((req) => {
-            if (req == 'require') implArgs.push(require);
-            else if (req == 'exports') implArgs.push(exports);
-            else implArgs.push(require(req));
-        })
+            if (req == 'require') {
+                implArgs.push(require);
+            }
+            else if (req == 'exports') {
+                implArgs.push(exports);
+            }
+            else if (req in window.defineLibs) {
+                implArgs.push(window.defineLibs[req]());
+            }
+            else {
+                implArgs.push(require(req));
+            }
+        });
         impl.apply(null, implArgs);
         return exports;
     });
